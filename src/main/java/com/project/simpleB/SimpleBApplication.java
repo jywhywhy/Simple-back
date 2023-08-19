08f0bede27f39cd6dcc8602f61ae8c3f75707926
song <jefkdjsf2@naver.com>
2023-08-19 22:42:10 +0900

    init

---
+++ src/main/java/com/project/simpleB/SimpleBApplication.java
@@ -0,0 +1,13 @@
+package com.project.simpleB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/simpleB/SimpleBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.simpleB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
